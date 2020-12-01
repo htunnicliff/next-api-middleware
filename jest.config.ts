cda9a75150af9eae4d7ee11471c8a99e1ac4f7aa
Hunter Tunnicliff <htunnicliff@hey.com>
2020-12-01 14:37:41 -0800

    Add codecov action

--- jest.config.ts
+++ jest.config.ts
@@ -34,7 +34,7 @@ export default {
   coverageProvider: "v8",
 
   // A list of reporter names that Jest uses when writing coverage reports
-  coverageReporters: ["xml", "text"],
+  coverageReporters: ["text", "json"],
 
   // An object that configures minimum threshold enforcement for coverage results
   // coverageThreshold: undefined,
