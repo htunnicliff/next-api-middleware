be0eeb7d70330740fe3b564262ed0e733c64e013
Hunter Tunnicliff <htunnicliff@interos.ai>
2021-02-19 10:23:25 -0800

    Add tests for ecosystem library, cors

---
+++ lib/ecosystem.spec.ts
@@ -0,0 +1,29 @@
+import { ExpressMiddleware, NextMiddleware, use } from ".";
+import cors from "cors";
+import { IncomingMessage, ServerResponse } from "http";
+import { Socket } from "net";
+
+describe("cors", () => {
+  it("works alongside Next Middleware", async () => {
+    expect.assertions(3);
+
+    const middleware = use(async (_req, res, next) => {
+      // No CORS headers yet
+      expect(res.hasHeader("Access-Control-Allow-Origin")).toBe(false);
+      await next();
+      // CORS headers applied
+      expect(res.getHeader("Access-Control-Allow-Origin")).toBe("*");
+    }, cors() as ExpressMiddleware);
+
+    // Create mocks
+    const handler = jest.fn(async (_req, res) => {
+      // API handler should have CORS headers
+      expect(res.getHeader("Access-Control-Allow-Origin")).toBe("*");
+    });
+    const req = new IncomingMessage(new Socket());
+    const res = new ServerResponse(req);
+
+    // Execute middleware with handler
+    await middleware(handler)(req as any, res as any);
+  });
+});
