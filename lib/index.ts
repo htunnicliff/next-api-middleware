0df078bb2448d8449dffdff852890590f391662c
Hunter Tunnicliff <htunnicliff@hey.com>
2022-02-06 11:48:44 -0800

    Remove `node:` protocol from imports

--- lib/index.ts
+++ lib/index.ts
@@ -1,4 +1,4 @@
-import type { IncomingMessage, ServerResponse } from "node:http";
+import type { IncomingMessage, ServerResponse } from "http";
 import type { NextApiRequest, NextApiResponse } from "next";
 
 export interface NextMiddleware {
